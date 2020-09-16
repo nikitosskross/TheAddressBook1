20a62f9ae5891ce9a278111bd0d8e4c67713ed99
nikitosskross <nikitosskross@yandex.ru>
2020-09-16 18:39:42 +0300

    AddressBook

--- src/main/java/addressBook/Address.java
+++ src/main/java/addressBook/Address.java
@@ -1,4 +1,45 @@
 package addressBook;
 
+import java.util.Objects;
+
 public class Address {
+    private String nameStreet;
+    private int houseNumber;
+    private int apartmentNumber;
+
+    public Address (String nameStreet, int houseNumber, int apartmentNumber) {
+        this.nameStreet = nameStreet;
+        this.houseNumber = houseNumber;
+        this.apartmentNumber = apartmentNumber;
+    }
+
+    public String getNameStreet() {
+        return nameStreet;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public int getApartmentNumber() {
+        return apartmentNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "улица " + nameStreet + " дом " + houseNumber + " квартира " + apartmentNumber + System.lineSeparator();
+    }
+
+    @Override
+    public boolean equals(Object x) {
+        if (this == x) return true;
+        if (x == null || getClass() != x.getClass()) return false;
+        Address address = (Address) x;
+        return nameStreet.equals(address.nameStreet) && houseNumber == address.houseNumber && apartmentNumber == address.apartmentNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameStreet, houseNumber, apartmentNumber);
+    }
 }
--- src/main/java/addressBook/AddressBook.java
+++ src/main/java/addressBook/AddressBook.java
@@ -1,4 +1,49 @@
 package addressBook;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class AddressBook {
+    private HashMap<String, Address> addressBook = new HashMap<String, Address>();
+
+    public void addToBook(String name, Address location) {
+
+        addressBook.put(name, location);
+    }
+
+    public void delete(String name) {
+
+        addressBook.remove(name);
+    }
+
+    public void changeAddress(String name, Address newAddress) {
+
+        addressBook.replace(name, newAddress);
+    }
+
+    public Address personOfBook(String name) {
+
+        return addressBook.get(name);
+    }
+
+    public List<String> listPersonByStreet(String street) {
+
+        List<String> person = new ArrayList<>();
+        addressBook.forEach((name, address) -> {
+            if (address.getNameStreet().equals(street))
+                person.add(name);
+        });
+        return person;
+    }
+
+    public List<String> listPersonByHouse(String street, int number) {
+
+        List<String> person = new ArrayList<>();
+        addressBook.forEach((name, address) -> {
+            if (address.getNameStreet().equals(street) && address.getHouseNumber() == number)
+                person.add(name);
+        });
+        return person;
+    }
 }
